1ac2f9998d8e0705cea7ef6a6b00609f7b35c14d
Vlada FKZ Laptop <vlada030@gmail.com>
2021-02-12 11:57:04 +0100

    nakon unosa sastojka, window pravi mali scroll da se vidi da je nesto ucitano

--- src/components/SmoothieList.js
+++ src/components/SmoothieList.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+import smoothscroll from 'smoothscroll-polyfill';
 import Smoothie from './Smoothie';
 import Loading from './Loading';
 import {smoothieList} from '../assets/languages/smoothieAllLang';
 import { useGlobalContext } from '../context';
+// koriguje window.behavior: 'smooth' da radi na svim browserima , UKLJUICUJUCI I SAFARI I STARIJE 
+smoothscroll.polyfill();
 
 const SmoothieList = () => {
 
   const {loading, smoothies, page, englishLang} = useGlobalContext();
+  const sectionScroll = useRef();
   
   const langOption = englishLang ? smoothieList.en : smoothieList.sr;
+
+  useEffect(() => {
+    //const isAndroid = /(android)/i.test(navigator.userAgent);
+    const isMobile = window.innerWidth < 800;
+
+    if (!loading && isMobile) {
+      window.scrollTo({
+        top: 100,
+        behavior: 'smooth'
+      });
+
+    }
+  }, [loading]);
 
   if (loading) {
     return <Loading />
@@ -21,7 +38,7 @@ const SmoothieList = () => {
   }
 
   return (
-    <section className='section'>
+    <section className='section' ref={sectionScroll}>
       <h2 className='section-title colorized colorized--red'>smoothies</h2>
       <div className='smoothies-center'>
         {smoothies[page].map(item => {
